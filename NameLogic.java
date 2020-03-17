a15d22674dfd8d37bbc756e2dbadc49f7020d974
Faozanap10 <61790554+Faozanap10@users.noreply.github.com>
2020-03-17 17:11:39 +0700

    Create NameLogic.java

---
+++ NameLogic.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihanformjava.function;
+
+import latihanformjava.Model.ModelProgram;
+
+/**
+ *
+ * @author faozan
+ */
+public class NameLogic {
+    
+    public String returnName (ModelProgram modelProgram){
+        
+        if(modelProgram.getName().equals(""))
+        {
+            return "Your name couldn't be empty!";
+        }else{
+            return "Hello, you must be " + modelProgram.getName();
+      
+    
+        
+      
+    }
+    
+        
+   
+            
+    }
+}
