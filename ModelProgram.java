38bbffd8d334bd457c989f2cb5566b0c90f68164
Faozanap10 <61790554+Faozanap10@users.noreply.github.com>
2020-03-17 17:10:43 +0700

    Create ModelProgram.java

---
+++ ModelProgram.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihanformjava.Model;
+
+/**
+ *
+ * @author faozan
+ */
+public class ModelProgram {
+    
+    private String name;
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+
+
+//    public String getBlank() {
+//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+//    }
+    }
+    
+
